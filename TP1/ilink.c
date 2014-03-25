d1585852d98c7132d09b12cbecd8b697cd5abef0
Bruno Ferreira <chalkos@chalkos.net>
2014-03-25 23:15:21 +0000

    links a funcionar excepto para páginas com caracteres estranhos...

--- TP1/ilink.c
+++ TP1/ilink.c
@@ -5,6 +5,8 @@
 #include "ilink.h"
 
 static void string_replace(char* str, const char find, const char replace){
+    if(!str) return;
+    
     while(*str != '\0'){
         if(*str == find)
             *str = replace;
@@ -49,7 +51,7 @@ void ilink_set_texto(ILink* ilink, char* str){
 
     ilink->texto = (char*) malloc( strlen(str)+1 );
     strcpy( ilink->texto, str);
-    string_replace(ilink->texto, ' ', '_');
+    //string_replace(ilink->texto, ' ', '_');
 }
 
 void ilink_set_parenteses(ILink* ilink, char* str){
@@ -76,6 +78,10 @@ void ilink_set_apresentar(ILink* ilink, char* str){
 
     //recebe:
     //coisas que aparecerem depois do pipe
+    //\ \t
+    
+    while(*str == ' ') //retirar espaços em branco no início
+        str++;
 
     ilink->apresentar = (char*) malloc( strlen(str)+1 );
     strcpy( ilink->apresentar, str );
@@ -93,14 +99,53 @@ void ilink_set_resto(ILink* ilink, char* str){
 // mostra o que tem na variavel ilink
 void ilink_print(ILink* ilink){
     //printf("<a href=\"https://en.wikipedia.org/wiki/%s", "dummy");
-    printf("especial: %s\n", ilink->especial);
-    printf("virgula: %s\n", ilink->virgula);
-    printf("apresentar: %s\n", ilink->apresentar);
-    printf("resto: %s\n", ilink->resto);
-    printf("parenteses: %s\n", ilink->parenteses);
-    printf("texto: %s\n\n", ilink->texto);
+    //printf("especial: %s\n", ilink->especial);
+    //printf("virgula: %s\n", ilink->virgula);
+    //printf("apresentar: %s\n", ilink->apresentar);
+    //printf("resto: %s\n", ilink->resto);
+    //printf("parenteses: %s\n", ilink->parenteses);
+    //printf("texto: %s\n\n", ilink->texto);
+    
 
+    printf("<a href=\"https://en.wikipedia.org/wiki/");
+    
+    string_replace(ilink->virgula, ' ', '_');
+    string_replace(ilink->parenteses, ' ', '_');
+    string_replace(ilink->texto, ' ', '_');
 
+    if( ilink->especial )
+        printf("%s:",ilink->especial);
+    
+    if( ilink->parenteses )
+        printf("%s%s", ilink->texto, ilink->parenteses);
+    else if( ilink->virgula )
+        printf("%s%s", ilink->texto, ilink->virgula);
+    else
+        printf("%s", ilink->texto);
+
+    printf("\">");
+
+    string_replace(ilink->virgula, '_', ' ');
+    string_replace(ilink->parenteses, '_', ' ');
+    string_replace(ilink->texto, '_', ' ');
+    
+    if(ilink->apresentar == NULL){
+        // mostrar tudo tal como está
+        if( ilink->parenteses )
+            printf("%s%s%s", ilink->texto, ilink->parenteses, ilink->resto);
+        else if( ilink->virgula )
+            printf("%s%s%s", ilink->texto, ilink->virgula, ilink->resto);
+        else
+            printf("%s%s", ilink->texto, ilink->resto);
+    }else if( ilink->apresentar != NULL && ilink->apresentar[0] == '\0' ){
+        // ocultar automáticamente os parenteses ou virgula
+        printf("%s%s", ilink->texto, ilink->resto);
+    } else {
+        // mostrar a parte de apresentacao
+        printf("%s", ilink->apresentar);
+    }
+    
+    printf("</a>\n\n");
 }
 
 // liberta memória associada ao ilink
